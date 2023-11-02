c143b34303582535e5b63e04a03caf1b002945c9
Ken <keniwane@gmail.com>
2023-11-02 15:11:44 -0700

    utilized bcrypt, and fixed database connection

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -1,23 +1,53 @@
 import React from 'react';
-import { useForm } from 'react-hook-form';
+import { useForm, Controller } from 'react-hook-form';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { GoogleLogin } from '@react-oauth/google';
 import { authenticateUser, setCredentials } from '../slices/authSlice';
+import { unwrapResult } from '@reduxjs/toolkit';
+import { manualLogin, signupUser } from '../slices/authSlice';
 import './login.css';
 const Login = () => {
-  const { register, handleSubmit } = useForm();
+  const { control: loginControl, handleSubmit: handleLoginSubmit } = useForm();
+
+  const { control: signupControl, handleSubmit: handleSignupSubmit } = useForm();
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const onLogin = (data) => {
-    navigate('/home');
+  const onLogin = (credentials) => {
+    dispatch(manualLogin(credentials))
+      .unwrap()
+      .then((result) => {
+        console.log('Login successful:', result);
+        navigate('/home');
+      })
+      .catch((error) => {
+        console.error('Login failed:', error);
+        const formElement = document.querySelector('.auth-form');
+        formElement.classList.add('shake-animation');
+
+        formElement.addEventListener(
+          'animationend',
+          () => {
+            formElement.classList.remove('shake-animation');
+          },
+          { once: true }
+        );
+      });
   };
 
-  const onSignup = (data) => {
-    navigate('/home');
+  const onSignup = (signupData) => {
+    console.log('signupData', signupData);
+    dispatch(signupUser(signupData))
+      .unwrap()
+      .then((result) => {
+        console.log('Signup successful:', result);
+        navigate('/home');
+      })
+      .catch((error) => {
+        console.error('Signup failed:', error);
+      });
   };
-
   const handleCredentialResponse = async (response) => {
     const googleData = {
       tokenId: response.credential,
@@ -45,50 +75,81 @@ const Login = () => {
     <div className='splash-container'>
       <div className='auth-container'>
         {/* Login Form */}
-        <form onSubmit={handleSubmit(onLogin)} className='auth-form'>
+        <form
+          onSubmit={handleLoginSubmit((data) => {
+            onLogin(data);
+          })}
+          className='auth-form'
+        >
           <h2>Login</h2>
+
           <div className='input-group'>
             <label htmlFor='email'>Email</label>
-            <input type='email' id='email' className='form-input' {...register('email')} required />
-          </div>
-          <div className='input-group'>
-            <label htmlFor='password'>Password</label>
-            <input
-              type='password'
-              id='password'
-              className='form-input'
-              {...register('password')}
-              required
+            <Controller
+              name='email'
+              control={loginControl}
+              defaultValue=''
+              rules={{ required: true }}
+              render={({ field }) => (
+                <input type='email' id='email' className='form-input' {...field} />
+              )}
             />
           </div>
+
+          <div className='input-group'>
+            <label htmlFor='password'>Password</label>
+            <Controller
+              name='password'
+              control={loginControl}
+              defaultValue=''
+              rules={{ required: true }}
+              render={({ field }) => (
+                <input type='password' id='password' className='form-input' {...field} />
+              )}
+            />
+          </div>
+
           <button type='submit' className='auth-button'>
             Login
           </button>
         </form>
 
         {/* Signup Form */}
-        <form onSubmit={handleSubmit(onSignup)} className='auth-form'>
+        <form
+          onSubmit={handleSignupSubmit((data) => {
+            console.log('Form submitted with data:', data);
+            onSignup(data);
+          })}
+          className='auth-form'
+        >
           <h2>Sign Up</h2>
+
           <div className='input-group'>
-            <label htmlFor='signup-email'>Email</label>
-            <input
-              type='email'
-              id='signup-email'
-              className='form-input'
-              {...register('email')}
-              required
+            <label htmlFor='email'>Email</label>
+            <Controller
+              name='signup_email'
+              control={signupControl}
+              defaultValue=''
+              rules={{ required: true }}
+              render={({ field }) => (
+                <input type='email' id='signup_email' className='form-input' {...field} />
+              )}
             />
           </div>
+
           <div className='input-group'>
-            <label htmlFor='signup-password'>Password</label>
-            <input
-              type='password'
-              id='signup-password'
-              className='form-input'
-              {...register('password')}
-              required
+            <label htmlFor='password'>Password</label>
+            <Controller
+              name='signup_password'
+              control={signupControl}
+              defaultValue=''
+              rules={{ required: true }}
+              render={({ field }) => (
+                <input type='password' id='signup_password' className='form-input' {...field} />
+              )}
             />
           </div>
+
           <button type='submit' className='auth-button'>
             Sign Up
           </button>
@@ -107,7 +168,7 @@ const Login = () => {
         {/* <div className='header-gif'>
           <img src='path-to-your-gif.gif' alt='Animated GIF' />
         </div> */}
-        <h1>Welcome to Job Tracker</h1>
+        <h1>Welcome to ByunDL</h1>
         <p>Your personal tool for tracking job applications and interviews.</p>
         <ul>
           <li>Organize your job search</li>
--- server/controllers/signin.js
+++ server/controllers/signin.js
@@ -1,18 +1,19 @@
 import { query } from '../models/jobTrackerModels.js';
-
+import bcrypt from 'bcrypt';
 import 'dotenv';
 
 const getRandomInt = (max) => {
   return Math.floor(Math.random() * Math.floor(max));
 };
 
+const SALT_ROUNDS = 10;
+
 const handleGoogleLogin = async (payload, res) => {
   try {
     const { sub: googleId, email, name } = payload;
-    const password = googleId;
+    const password = await bcrypt.hash(googleId, SALT_ROUNDS);
     const username = email;
     const userQueryResult = await query('SELECT * FROM users WHERE username = $1', [username]);
-    console.log(userQueryResult);
     const _id = getRandomInt(2147483647);
 
     if (userQueryResult.rows.length > 0) {
@@ -31,4 +32,68 @@ const handleGoogleLogin = async (payload, res) => {
     res.status(500).json({ message: 'Internal server error', error: error.message });
   }
 };
-export { handleGoogleLogin };
+
+const manualLogin = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const username = email;
+
+    const userQueryResult = await query('SELECT * FROM users WHERE username = $1', [username]);
+    console.log(userQueryResult);
+    if (userQueryResult.rows.length > 0) {
+      const user = userQueryResult.rows[0];
+      const passwordIsValid = await bcrypt.compare(password, user.password);
+
+      if (passwordIsValid) {
+        return res.status(200).json({
+          message: 'Login successful!',
+        });
+      } else {
+        return res.status(401).json({
+          message: 'Invalid email or password!',
+        });
+      }
+    } else {
+      return res.status(401).json({
+        message: 'Invalid email or password!',
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+const signup = async (req, res, next) => {
+  try {
+    const { signup_email, signup_password } = req.body;
+    console.log(req.body);
+    if (!signup_email || !signup_password) {
+      return res.status(400).json({
+        message: 'Email and password required!',
+      });
+    }
+    const username = signup_email;
+    const hashedPassword = await bcrypt.hash(signup_password, SALT_ROUNDS);
+
+    const userQueryResult = await query('SELECT * FROM users WHERE username = $1', [username]);
+    if (userQueryResult.rows.length > 0) {
+      throw new Error('Username is taken!');
+    } else {
+      const _id = getRandomInt(2147483647);
+      const newUserQueryResult = await query(
+        'INSERT INTO users (_id, username, password) VALUES ($1, $2, $3) RETURNING *',
+        [_id, username, hashedPassword]
+      );
+
+      const newUser = newUserQueryResult.rows[0];
+      const { password, ...userWithoutPassword } = newUser;
+      return res.status(201).json({
+        message: 'Signup successful!',
+        user: userWithoutPassword,
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+export { handleGoogleLogin, manualLogin, signup };
